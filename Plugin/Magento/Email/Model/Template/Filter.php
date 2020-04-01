76a14a3cd04b5e0281dcb3c97a32113889cf6f6e
Andreas Pointner <andreas.pointner@copex.io>
2020-04-01 20:34:56 +0200

    [TASK] check app state

--- Plugin/Magento/Email/Model/Template/Filter.php
+++ Plugin/Magento/Email/Model/Template/Filter.php
@@ -1,35 +1,54 @@
 <?php
 
-
 namespace CopeX\CacheEmailCssForDevelopers\Plugin\Magento\Email\Model\Template;
 
 use Magento\Framework\App\CacheInterface;
+use Magento\Framework\App\State;
+
 /**
  * Class Filter
- *
  * @package CopeX\CacheEmailCssForDevelopers\Plugin\Magento\Email\Model\TemplateContent
  */
 class Filter
 {
 
-
-    /** @var \Magento\Framework\App\CacheInterface */
+    /** @var CacheInterface */
     protected $cache;
+    /**
+     * @var State
+     */
+    protected $state;
 
-    public function __construct(CacheInterface $cache)
+    /**
+     * Filter constructor.
+     * @param State          $state
+     * @param CacheInterface $cache
+     */
+    public function __construct(State $state, CacheInterface $cache)
     {
+        $this->state = $state;
         $this->cache = $cache;
     }
 
+    /**
+     * @param \Magento\Email\Model\Template\Filter $subject
+     * @param \Closure                             $proceed
+     * @param array                                $files
+     * @return string
+     */
     public function aroundGetCssFilesContent(
         \Magento\Email\Model\Template\Filter $subject,
         \Closure $proceed,
         array $files
     ) {
-        $cacheKey = "EMAIL_FILTER_" . implode("_", $files);
-        if (!$css = $this->cache->load($cacheKey)) {
+        if ($this->state->getMode() !== State::MODE_PRODUCTION) {
+            $cacheKey = "EMAIL_FILTER_" . implode("_", $files);
+            if (!$css = $this->cache->load($cacheKey)) {
+                $css = $proceed($files);
+                $this->cache->save($css, $cacheKey, ["config"]);
+            }
+        } else {
             $css = $proceed($files);
-            $this->cache->save($css, $cacheKey, ["config"]);
         }
         return $css;
     }
